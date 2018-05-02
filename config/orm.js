b2f58007dd025330d52b493d051e8f16558921de
josephemswiler <josephemswiler@gmail.com>
2018-05-02 10:05:58 -0500

    orm create, read, update

--- config/orm.js
+++ config/orm.js
@@ -3,69 +3,59 @@ const connection = require('./connection.js')
 // insertOne()
 // updateOne()
 
-let orm = {
-    selectAll: function(table, callback) {
-
-      let queryString = 'SELECT * FROM ??;'
-
-      connection.query(queryString, table, function(err, result) {
-        if (err) {
-          throw err
-        }
-        callback(result)
-      })
-    },
-    insertOne: function(table, cols, vals, cb) { //here
-      var queryString = "INSERT INTO ??"
-  
-      queryString += " (";
-      queryString += cols.toString();
-      queryString += ") ";
-      queryString += "VALUES (";
-      queryString += printQuestionMarks(vals.length);
-      queryString += ") ";
-  
-      console.log(queryString);
-  
-      connection.query(queryString, vals, function(err, result) {
-        if (err) {
-          throw err;
-        }
-  
-        cb(result);
-      });
-    },
-    // An example of objColVals would be {name: panther, sleepy: true}
-    update: function(table, objColVals, condition, cb) {
-      var queryString = "UPDATE " + table;
-  
-      queryString += " SET ";
-      queryString += objToSql(objColVals);
-      queryString += " WHERE ";
-      queryString += condition;
-  
-      console.log(queryString);
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-  
-        cb(result);
-      });
-    },
-    delete: function(table, condition, cb) {
-      var queryString = "DELETE FROM " + table; //don't do this, use string interpolation
-      queryString += " WHERE ";
-      queryString += condition;
-  
-      connection.query(queryString, function(err, result) {
-        if (err) {
-          throw err;
-        }
-  
-        cb(result);
-      });
+class ObjectRelationalMapping {
+    constructor(table, columns) {
+        this.table = table
+        this.columns = columns
     }
-  };
+
+    selectAll(callback) {
+
+        let queryString = 'SELECT * FROM ??;'
+
+        connection.query(queryString, this.table, function (err, result) {
+            if (err) {
+                throw err
+            }
+            callback(result)
+        })
+    } //selectAll
+
+    insertOne(text, list, callback) {
+
+        let queryString = `INSERT INTO ?? (??) VALUES (?, ?);`
+
+        connection.query(queryString, [this.table, this.columns, text, list], function (err, result) {
+            if (err) {
+                throw err
+            }
+            callback(result)
+        })
+    } //insertOne
+
+    updateOne(itemId, text, list, callback) {
+
+        let queryString = 'UPDATE ?? SET ?, ? WHERE ?'
+
+        let textUpdate = {}
+        textUpdate[this.columns[0]] = text
+
+        let listUpdate = {}
+        listUpdate[this.columns[1]] = list
+
+        let reference = {
+            id: itemId
+        }
+
+        connection.query(queryString, [this.table, textUpdate, listUpdate, reference], function (err, result) {
+            if (err) {
+                throw err
+            }
+            callback(result)
+        })
+    } //updateOne
+}
+
+let orm = new ObjectRelationalMapping('cards', ['text', 'list'])
 
 module.exports = orm
--- models/trello.js
+++ models/trello.js
@@ -1,5 +1,13 @@
 const orm = require('./../config/orm.js')
 
-orm.selectAll('cards', function(result) {
+orm.selectAll(function(result) {
+    console.log(result)
+})
+
+// orm.insertOne('Make ORM a class', 'Doing', function(result) {
+//     console.log(result)
+// })
+
+orm.updateOne(6, 'Make ORM a class', 'Ideas', function(result) {
     console.log(result)
 })
