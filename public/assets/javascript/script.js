6edad0a9050cf0dfcca57950604d2f182e238368
josephemswiler <josephemswiler@gmail.com>
2018-05-03 12:45:12 -0500

    manual card add

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,9 +1,4 @@
 (function () {
-    console.log('hello world')
-
-    let cards = []
-
-
     //dynamically set max-height of list based on window size
     function setHeight() {
         let innerHeight = $(window).height() - 200
@@ -24,10 +19,39 @@
             .toggleClass('lighten-3 lighten-1 underlined')
     })
 
+    $(document).on('click', '.add-card-btn', function () {
+        let p = $('<p>')
+            .addClass('open-card')
+            .attr('contenteditable', 'true');
+        let content = $('<div>')
+            .addClass('card-content')
+            .append(p)
+        let div = $('<div>')
+            .addClass('card inner-card')
+            .append(content)
+        let close = $('<i>')
+            .addClass('fas fa-times')
+        let button = $('<button>')
+            .addClass('open-card-btn waves-effect waves-light btn green darken-1 white-text')
+            .text('Add')
+        let wrapper = $('<div>')
+            .append(div)
+            .append(button, close)
+
+        $('.card-data').append(wrapper)
+        $('.open-card').focus().select()
+    })
+
     
 
-    $(document).on('click', '.add-card-btn', function () {
-        console.log('hello')
+    $(document).on('click', '.open-card-btn', function () {
+        console.log($('.open-card').text())
+
+        $(this).remove()
+        $('.fa-times').remove()
+        
+        $('.open-card')
+            .attr('contenteditable', 'false');
     })
 
 
