ec716d238ec051f6b9ac4a95e5c04532338f16c5
josephemswiler <josephemswiler@gmail.com>
2018-05-03 11:36:47 -0500

    responsive max-height of cards

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,13 +1,34 @@
-(function() {
+(function () {
     console.log('hello world')
 
-    $('.sidenav').sidenav()
+    let cards = []
 
+
+    //dynamically set max-height of list based on window size
+    function setHeight() {
+        let innerHeight = $(window).height() - 200
+        $('.card-data').css('max-height', innerHeight)
+    }
+    $(document).ready(setHeight())
+
+    $(window).resize(function () {
+        setHeight()
+    })
+
+    //materialize responsive elements
+    $('.sidenav').sidenav()
     $('.tooltipped').tooltip()
 
-    $('.add-card-btn-wrapper').hover(function() {
-        $( this )
-          .toggleClass('lighten-3 lighten-1 underlined')
+    $('.add-card-btn-wrapper').hover(function () {
+        $(this)
+            .toggleClass('lighten-3 lighten-1 underlined')
     })
+
+    
+
+    $(document).on('click', '.add-card-btn', function () {
+        console.log('hello')
+    })
+
 
 })() //IIFE
