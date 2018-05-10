54593464ff59c4bf24ac9a1c721dd69c198dd2f2
josephemswiler <josephemswiler@gmail.com>
2018-05-10 12:46:33 -0500

    mobile touch UI

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -37,7 +37,6 @@
             drop: function (event, ui) {
                 $(this).children('.card-data').append($(ui.draggable))
                 updateList($(this).closest('.outer-card')[0].dataset.id, $(ui.draggable)[0].dataset.id, $(ui.draggable).text().trim())
-                checkHeight()
             }
         })
 
@@ -49,7 +48,6 @@
             drop: function (event, ui) {
                 $(this).siblings('.card-content').children('.card-data').append($(ui.draggable))
                 updateList($(this).closest('.outer-card')[0].dataset.id, $(ui.draggable)[0].dataset.id, $(ui.draggable).text().trim())
-                checkHeight()
             }
         })
     }
@@ -195,9 +193,6 @@
                 .addClass('drag-wrapper ui-draggable ui-draggable-handle')
                 .draggable('enable')
         }
-
-
-
         $(this)
             .remove()
         $('.close-open-card')
@@ -726,6 +721,38 @@
 
             icon.toggleClass('flip')
         }
+    })
+
+    $(document).mouseup(function() {
+        checkHeight()
+    })
+
+    $(document).ready(function() {
+        function touchHandler(event) {
+            var touch = event.changedTouches[0];
+        
+            var simulatedEvent = document.createEvent("MouseEvent");
+                simulatedEvent.initMouseEvent({
+                touchstart: "mousedown",
+                touchmove: "mousemove",
+                touchend: "mouseup"
+            }[event.type], true, true, window, 1,
+                touch.screenX, touch.screenY,
+                touch.clientX, touch.clientY, false,
+                false, false, false, 0, null);
+        
+            touch.target.dispatchEvent(simulatedEvent);
+            event.preventDefault();
+        }
+        
+        function init() {
+            document.addEventListener("touchstart", touchHandler, true);
+            document.addEventListener("touchmove", touchHandler, true);
+            document.addEventListener("touchend", touchHandler, true);
+            document.addEventListener("touchcancel", touchHandler, true);
+        }
+
+        init()
     })
 
     //db CRUD Functions
